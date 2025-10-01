fe1e7d4cc8cbe8e30ad4a879106fd41f8af5b8cd
Barbara Calas <96188300+bcalas78@users.noreply.github.com>
2025-10-01 16:22:42 +0200

    Revert "Feature/comment routes"

--- src/server.js
+++ src/server.js
@@ -1,47 +1,24 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
 const sequelize = require("./config/database");
-const User = require("./models/User");
-const Project = require("./models/Project");
-const Task = require("./models/Task");
-const Comment = require("./models/Comment");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+// Route test
+app.get("/", (req, res) => {
+    res.send("API Kanban is working 🚀");
+});
 
 const PORT = process.env.PORT || 4000;
 
 // Connexion DB et lancement du serveur
-sequelize
-    .sync({alter: true})
+sequelize.authenticate()
     .then(() => {
-        console.log("Base de données synchronisée");
-
-        app.listen(PORT, () => { 
-            console.log(`Serveur lancé sur http://localhost:${PORT}`)
-        });
+        console.log("Connexion réussie à PostgreSQL");
+        app.listen(PORT, () => console.log(`Serveur lancé sur http://localhost:${PORT}`));
     })
-    .catch((err) => {
-        console.error("Erreur lors de la synchronisation de la DB:", err)
-    });
-
-// Route test
-app.get("/", (req, res) => {
-    res.send("Bienvenue sur le projet Kanban 🚀");
-});
-
-const authRoutes = require("./routes/auth");
-app.use("/auth", authRoutes);
-
-const projectRoutes = require("./routes/projects");
-app.use("/projects", projectRoutes);
-
-const taskRoutes = require("./routes/tasks");
-app.use(taskRoutes);
-
-const commentRoutes = require("./routes/comment");
-app.use(commentRoutes);
+    .catch(err => console.error("Erreur lors de la connexion à la DB:", err));
